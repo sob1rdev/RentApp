348f0f1b9aee317d18290c06a50f61d2d70fc7c8
Farrukh <adler4313@gmail.com>
2024-09-23 03:37:57 +0500

    MINOR: Defined basic relationships

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ad extends Model
 {
     use HasFactory;
+
+    public function branch(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function status(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Status::class);
+    }
 }
